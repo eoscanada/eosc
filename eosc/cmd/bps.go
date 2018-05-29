94a9434b625ed55a7d9d6657b6994c227d6317df
billettc <charles@dix975.com>
2018-05-29 11:10:45 -0400

    added bps registration

--- eosc/cmd/bps.go
+++ eosc/cmd/bps.go
@@ -25,14 +25,4 @@ var bpsCmd = &cobra.Command{
 
 func init() {
 	RootCmd.AddCommand(bpsCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// bpsCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// bpsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
