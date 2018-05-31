2eed142e9e38933315b4d52da7d6c67fe5341cd5
Alexandre Bourget <alex@bourget.cc>
2018-05-30 23:46:25 -0400

    Updated dependencies.

--- vendor/github.com/dgiagio/getpass/getpass.go
+++
@@ -1,8 +0,0 @@
-// Package getpass provides support for reading passwords on the command-line.
-package getpass
-
-// GetPassword displays a prompt to the user and reads the password from
-// standard input without echoing on the screen.
-func GetPassword(prompt string) (pass string, err error) {
-	return getPassword(prompt)
-}
