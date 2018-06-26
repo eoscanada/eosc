0ccde3e52457d6579be614c0b6a5eb7b8c64736d
Alexandre Bourget <alex@bourget.cc>
2018-06-26 01:52:59 -0400

    Implemented the eosio forum (with a temporary account name) interaction methods.
    Doesn't yet allow to read chain contents, .. that'll come!

---
+++ eosc/cmd/forumPost.go
@@ -0,0 +1,75 @@
+// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"encoding/json"
+
+	"github.com/eoscanada/eos-go"
+	"github.com/eoscanada/eos-go/forum"
+	"github.com/pborman/uuid"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var forumPostCmd = &cobra.Command{
+	Use:   "post [from_account] [message]",
+	Short: "Post a message",
+	Args:  cobra.ExactArgs(2),
+	Run: func(cmd *cobra.Command, args []string) {
+		accountName := eos.AccountName(args[0])
+		message := args[1]
+		title := viper.GetString("forum-post-cmd-title")
+		certify := viper.GetBool("forum-post-cmd-certify")
+		newUUID := uuid.New()
+
+		metadata := viper.GetString("forum-post-cmd-metadata")
+		if metadata != "" {
+			var dump interface{}
+			err := json.Unmarshal([]byte(metadata), &dump)
+			errorCheck("--metadata is not valid JSON", err)
+		} else {
+			metadataBytes, _ := json.Marshal(map[string]interface{}{
+				"type": viper.GetString("forum-post-cmd-type"),
+			})
+			metadata = string(metadataBytes)
+		}
+
+		replyTo := eos.AccountName(viper.GetString("forum-post-cmd-reply-to"))
+		replyToUUID := viper.GetString("forum-post-cmd-reply-to-uuid")
+
+		api := apiWithWallet()
+		pushEOSCActions(api,
+			forum.NewPost(accountName, newUUID, title, message, replyTo, replyToUUID, certify, metadata),
+		)
+	},
+}
+
+func init() {
+	forumCmd.AddCommand(forumPostCmd)
+
+	forumPostCmd.Flags().StringP("title", "", "", "The title for the post. None by default")
+	forumPostCmd.Flags().BoolP("certify", "", false, "Certify that the contents of this message is true")
+	forumPostCmd.Flags().StringP("type", "", "chat", "Message type (added to json_metadata)")
+	forumPostCmd.Flags().StringP("metadata", "", "", "Additional metadata. Must be JSON-encoded. If present, takes precedences over --type")
+	forumPostCmd.Flags().StringP("reply-to", "", "", "Account name to reply to")
+	forumPostCmd.Flags().StringP("reply-to-uuid", "", "", "UUID from a previous post from the --repy-to account.")
+
+	for _, flag := range []string{"title", "certify", "type", "metadata", "reply-to", "reply-to-uuid"} {
+		if err := viper.BindPFlag("forum-post-cmd-"+flag, forumPostCmd.Flags().Lookup(flag)); err != nil {
+			panic(err)
+		}
+	}
+
+}
---
+++ eosc/cmd/forumVote.go
@@ -0,0 +1,51 @@
+// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"github.com/eoscanada/eos-go"
+	"github.com/eoscanada/eos-go/forum"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var forumVoteCmd = &cobra.Command{
+	Use:   "vote [voter] [proposition] [vote_value]",
+	Short: "Submit a vote from [voter] on the [proposition] with a [vote_value] agreed in the proposition.",
+	Args:  cobra.ExactArgs(3),
+	Run: func(cmd *cobra.Command, args []string) {
+
+		accountName := eos.AccountName(args[0])
+		proposition := args[1]
+		vote := args[2]
+		propositionHash := viper.GetString("forum-vote-cmd-hash")
+
+		api := apiWithWallet()
+		pushEOSCActions(api,
+			forum.NewVote(accountName, proposition, propositionHash, vote),
+		)
+	},
+}
+
+func init() {
+	forumCmd.AddCommand(forumVoteCmd)
+
+	forumPostCmd.Flags().StringP("hash", "", "", "Hash of the proposition, as defined by the proposition itself")
+
+	for _, flag := range []string{"hash"} {
+		if err := viper.BindPFlag("forum-vote-cmd-"+flag, forumPostCmd.Flags().Lookup(flag)); err != nil {
+			panic(err)
+		}
+	}
+}
