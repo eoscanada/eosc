3bb2c5e8c68a0d31801db8c682b4645692ff4428
Alexandre Bourget <alex@bourget.cc>
2018-06-28 11:17:47 -0400

    Updated bidname,

--- eosc/cmd/systemBidname.go
+++ eosc/cmd/systemBidname.go
@@ -8,27 +8,29 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var bidNewNameCmd = &cobra.Command{
-	Use:   "bidname [bidder_account_name] [newname] [bid_asset]",
-	Short: "Bidding a name with asset.",
-	Long: `Command to name bidding.
+var systemBidnameCmd = &cobra.Command{
+	Use:   "bidname [bidder_account_name] [premium_account_name] [bid quantity]",
+	Short: "Bid on a premium account name",
+	Long: `Bid on a premium account name
 
-bidder_account_name TEXT  The bidding account name(required)
-newname TEXT  The bidding name (required)
-bid_asset TEXT  The amount of EOS to bid (required)
-
-You can bidding a new name with:
+All fields are required. Example usage:
 
     eosc system bidname your_account_name eos "10.0000 EOS"
+
+Please note you could be locking up your funds in the name bidding
+auction if you don't intend to go through and being the highest
+bidder.
+
+Read https://steemit.com/eos/@eos-canada/everything-you-need-to-know-about-namespace-bidding-on-eos for more infos.
 `,
-	Args: cobra.MinimumNArgs(3),
+	Args: cobra.ExactArgs(3),
 	Run: func(cmd *cobra.Command, args []string) {
 		api := apiWithWallet()
 
 		bidder := eos.AccountName(args[0])
 		newname := eos.AccountName(args[1])
-		bidAsset, err := eos.NewAsset(args[2])
-		errorCheck("Error get bid", err)
+		bidAsset, err := eos.NewEOSAssetFromString(args[2])
+		errorCheck("bid amount invalid", err)
 
 		fmt.Printf("[%s] bidding for: %s , amount=%d precision=%d symbol=%s\n", bidder, newname, bidAsset.Amount, bidAsset.Symbol.Precision, bidAsset.Symbol.Symbol)
 
@@ -39,5 +41,5 @@ You can bidding a new name with:
 }
 
 func init() {
-	systemCmd.AddCommand(bidNewNameCmd)
+	systemCmd.AddCommand(systemBidnameCmd)
 }
