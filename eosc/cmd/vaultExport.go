77f5f573c4bbb0e3f9eda42347829468c368fe0f
John Adams <john@bringhub.com>
2018-08-16 08:36:34 -0400

    Adding in vault export command.  Addresses #10

---
+++ eosc/cmd/vaultExport.go
@@ -0,0 +1,23 @@
+// Copyright © 2018 EOS Canada <info@eoscanada.com>
+
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// vaultExportCommand represents the export command
+var vaultExportCommand = &cobra.Command{
+	Use:   "export",
+	Short: "Export private keys (and corresponding public keys) inside an eosc vault.",
+	Long:  `Export private keys (and corresponding public keys) inside an eosc vault.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		vault := mustGetWallet()
+
+		vault.PrintPrivateKeys()
+	},
+}
+
+func init() {
+	vaultCmd.AddCommand(vaultExportCommand)
+}
