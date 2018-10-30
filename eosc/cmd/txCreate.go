895da141bba2ba4715419fff8bb986f0bf59ce7e
Matthieu Vachon <matt@eoscanada.com>
2018-10-30 10:57:20 -0400

    Added flag `-f` (`--force-unique`) on `tx create`
    By using this flag, you ensure the created transaction will be unique.
    This work by creating a `eosio.null:nonce` action with the value
    being a generate random number.
    
    The nonce is generated using 48 bits entropy read from the random stream.
    This should be enough for this use cases of generating unique transaction.

--- eosc/cmd/txCreate.go
+++ eosc/cmd/txCreate.go
@@ -3,10 +3,15 @@
 package cmd
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"encoding/json"
+	"io"
 
 	eos "github.com/eoscanada/eos-go"
+	"github.com/eoscanada/eos-go/system"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var txCreateCmd = &cobra.Command{
@@ -18,6 +23,8 @@ var txCreateCmd = &cobra.Command{
 		action := toActionName(args[1], "action")
 		payload := args[2]
 
+		forceUnique := viper.GetBool("tx-create-cmd-force-unique")
+
 		var dump map[string]interface{}
 		err := json.Unmarshal([]byte(payload), &dump)
 		errorCheck("[payload] is not valid JSON", err)
@@ -26,14 +33,55 @@ var txCreateCmd = &cobra.Command{
 		actionBinary, err := api.ABIJSONToBin(contract, eos.Name(action), dump)
 		errorCheck("unable to retrieve action binary from JSON via API", err)
 
-		pushEOSCActions(api, &eos.Action{
-			Account:    contract,
-			Name:       action,
-			ActionData: eos.NewActionDataFromHexData([]byte(actionBinary)),
-		})
+		actions := []*eos.Action{
+			&eos.Action{
+				Account:    contract,
+				Name:       action,
+				ActionData: eos.NewActionDataFromHexData([]byte(actionBinary)),
+			}}
+
+		var contextFreeActions []*eos.Action
+		if forceUnique {
+			contextFreeActions = append(contextFreeActions, newNonceAction())
+		}
+
+		pushEOSCActionsAndContextFreeActions(api, contextFreeActions, actions)
 	},
+}
+
+func newNonceAction() *eos.Action {
+	return &eos.Action{
+		Account: eos.AN("eosio.null"),
+		Name:    eos.ActN("nonce"),
+		ActionData: eos.NewActionData(system.Nonce{
+			Value: hex.EncodeToString(generateRandomNonce()),
+		}),
+	}
+}
+
+func generateRandomNonce() []byte {
+	// Use 48 bits of entropy to generate a valid random
+	nonce := make([]byte, 6)
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		errorCheck("unable to correctly generate nonce", err)
+	}
+
+	return nonce
 }
 
 func init() {
 	txCmd.AddCommand(txCreateCmd)
+
+	txCreateCmd.Flags().BoolP(
+		"force-unique",
+		"f",
+		false,
+		"force the transaction to be unique. this will consume extra bandwidth and remove any protections against accidently issuing the same transaction multiple times",
+	)
+
+	for _, flag := range []string{"force-unique"} {
+		if err := viper.BindPFlag("tx-create-cmd-"+flag, txCreateCmd.Flags().Lookup(flag)); err != nil {
+			panic(err)
+		}
+	}
 }
