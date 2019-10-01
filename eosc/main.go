dd0e053aaf657f0fc111d34a58841b8da8406a6e
vd <34808607+vadim-di@users.noreply.github.com>
2019-10-01 23:07:02 +1000

    Update main.go

--- eosc/main.go
+++ eosc/main.go
@@ -7,7 +7,7 @@ import (
 	_ "github.com/eoscanada/eos-go/system"
 	_ "github.com/eoscanada/eos-go/token"
 
-	"github.com/vadim-di/eosc/eosc/cmd"
+	"github.com/eoscanada/eosc/eosc/cmd"
 )
 
 var version = "dev"
